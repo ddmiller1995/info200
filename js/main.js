1094bdd27c0d320b250d42b9d42d6c6f22460731
Dakota Miller <ddmiller1995@gmail.com>
2015-11-09 15:02:41 -0800

    Add intitial files

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/*
+    Name: Dakota Miller
+    Class: INFO 200
+    Description: Javascript for "Connect"
+*/
+
+
+"use strict";
+
+$(document).ready(function() {
+
+	$(".dropdown-menu a").click(function(event) {
+		console.log(event);
+		//$("body")
+	});
+
+	$("#work-button").click(function() {
+		$(this).css("display","none");
+		$("#work-options").css("display","block");
+	});
+
+	$("#hire-button").click(function() {
+		$(this).css("display","none");
+		$("#hire-options").css("display","block");
+	});
+
+
+});
